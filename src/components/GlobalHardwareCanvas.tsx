7ea987f2b8f8758a1dd8c1290fa9259947525b19
gourab354 <gourabd911@gmail.com>
2026-08-30 21:48:11 +0530

    Increase particle opacity on laptop screens to 0.6

--- src/components/GlobalHardwareCanvas.tsx
+++ src/components/GlobalHardwareCanvas.tsx
@@ -302,7 +302,7 @@ const MorphingParticles = () => {
         size={0.045}
         color="#4829BA" // Dark blue / Royal Purple
         transparent
-        opacity={0.25}
+        opacity={isMobile ? 0.25 : 0.6}
         sizeAttenuation
       />
     </points>
